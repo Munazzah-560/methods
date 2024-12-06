56ba61b7efd68801e25818dd6f0f71ff4f34e8f0
Lab Laptop <rams.dai230@gmail.com>
2024-12-06 22:20:59 +0500

    all files

---
+++ app.js
@@ -0,0 +1,63 @@
+            //Answer 1
+// let arr = [42,true,"hello, world",{name:"john",age:25,isStudent:false,}, false,3.14,"JavaScript is fun!"]
+// let newArr = arr.map((e, i) => {
+//     return e;
+// });
+// console.log(newArr);
+
+            //Answer 2
+// let arr = [42,true,"hello, world",{name:"john",age:25,isStudent:false,}, false,3.14,"JavaScript is fun!"]
+// let newArr = arr.filter((e,i)=>{
+//     return e, typeof e==='number'
+// })
+// console.log(newArr)
+
+            //Answer 3
+// let arr = [42,true,"hello, world",{name:"john",age:25,isStudent:false,}, false,3.14,"JavaScript is fun!"]
+// arr.forEach(e => {
+//     console.log(e);   
+// })
+
+            //Answer 4
+// let arr = [42,true,"hello, world",{name:"john",age:25,isStudent:false,}, false,3.14,"JavaScript is fun!"]
+// let element = arr.reduce((a, b) => {
+//   return a + b.num;
+// }, 10);
+
+// console.log(element)
+
+            //Answer 5
+// let arr = [42,true,"hello, world",{name:"john",age:25,isStudent:false,}, false,3.14,"JavaScript is fun!"]
+// let newArr = arr.find((e, i) => {
+//     return typeof e=== 'boolean';
+//   });
+// console.log(newArr);
+
+            //Answer 6
+// let arr = [42,true,"hello, world",{name:"john",age:25,isStudent:false,}, false,3.14,"JavaScript is fun!"]
+// let newArr = arr.findIndex((e,i)=>{
+//     return typeof e === 'object'
+// }) 
+// console.log(newArr)
+
+            //Answer 7
+// let arr = [42,true,"hello, world",{name:"john",age:25,isStudent:false,}, false,3.14,"JavaScript is fun!"]
+         
+// let newArr = arr.some((e,i)=>{
+//     return e === 42
+// });
+
+// console.log(newArr);
+
+            //Answer 8
+// let arr = ["hello, world","john" ,"JavaScript is fun!"]
+// let newArr = arr.every((e, i) => {
+//     return typeof e === 'string';
+//   });
+  
+//   console.log(newArr);
+
+
+
+
+
